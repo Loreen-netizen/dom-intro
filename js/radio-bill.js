eeb172d47305d23a49a8e26b6e07819e2efbb9d7
Loreen-netizen <loreenmuusha@gmail.com>
2020-04-09 13:12:35 +0200

    half of Radio Button Bill Widget done

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -1,23 +1,38 @@
 // get a reference to the sms or call radio buttons
-var billItemTypeRadioElemnt = document.querySelector(".billItemTypeRadio")
+var billItemTypeRadioElement = document.querySelector(".billItemTypeRadio")
 //get a reference to the add button
-var radioBillAddBtnElemnt = document.querySelector(".radioBillAddBtn")
-var callTotalTwoElemnt = document.querySelector(".callTotalTwo")
-var smsTotalTwoElemnt = document.querySelector(".smsTotalTwo")
+var radioBillAddBtnElement = document.querySelector(".radioBillAddBtn")
+var callTotalTwoElement = document.querySelector(".callTotalTwo")
+var smsTotalTwoElement = document.querySelector(".smsTotalTwo")
 
 //create a variable that will keep track of the total bill
-var totalTwoElemnt = document.querySelector(".totalTwo")
+var totalTwoElement = document.querySelector(".totalTwo")
 //add an event listener for when the add button is pressed
-function billTypeAndTotals(){
+function radioButtonBill () {
 var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
 if (checkedRadioBtn){
-    var billItemType = checkedRadioBtn.value;
+    var billItemType = checkedRadioBtn.value}
+    // alert(checkedRadioBtn.value);
+    var callsTotal = 0;
+    var smsTotal = 0;
+    // billItemType will be 'call' or 'sms'
 
-    totalTwoElemnt.innerHTML = billItemType;
-    alert("hello");}
+    if (billItemType === "call") {
+        callsTotal += 2.75
+    }
+    else if (billItemType === "sms") {
+        smsTotal += 0.75;
+    }
+    callTotalTwoElement.innerHTML = callsTotal.toFixed(2);
+    smsTotalTwoElement.innerHTML = smsTotal.toFixed(2);
+    var totalCost = callsTotal + smsTotal;
+    totalTwoElement.innerHTML = totalCost.toFixed(2);
+
+};
+
     // billItemType will be 'call' or 'sms'
     
-}
+    radioBillAddBtnElement.addEventListener("click", radioButtonBill);
 //in the event listener get the value from the billItemTypeRadio radio buttons
 
 // * add the appropriate value to the running total
