decbe24433860f5cd702b3068d2449387a8ef21e
Loreen-netizen <loreenmuusha@gmail.com>
2020-05-26 12:10:46 +0200

    bill with settings linked to factory function

---
+++ dom/settings-bill-dom.js
@@ -0,0 +1,65 @@
+var billItemTypeWithSettingsElement = document.querySelectorAll(".billItemTypeWithSettings");
+var callCostSettingElement = document.getElementById("callSetting");
+var smsCostSettingElement = document.getElementById("smsSetting");
+var warningLevelSettingElement = document.getElementById("warningSetting");
+var criticalLevelSettingElement = document.getElementById("criticalSetting");
+var billWithSettingsButtonElement = document.querySelector(".button-primary.billWithSettings");
+var updateSettingsElement = document.getElementById("updateSettingsButton");
+var callTotalSettingsElement = document.getElementById("callsTotal");
+var smsTotalSettingsElement = document.getElementById("smsTotal");
+var totalSettingsElement = document.getElementById("totalSettings");
+var callChecked = document.getElementById("calls");
+var smsChecked = document.getElementById("smses");
+let billWithSettingsInstance = billWithSettingsFunction();
+
+updateSettingsElement.onclick = function () {
+
+    billWithSettingsInstance.setCallCost(Number(callCostSettingElement.value));
+    billWithSettingsInstance.setSmsCost(Number(smsCostSettingElement.value));
+    billWithSettingsInstance.setWarningLevel(Number(warningLevelSettingElement.value));
+    billWithSettingsInstance.setCriticalLevel(Number(criticalLevelSettingElement.value));
+    changeClass();
+
+    billWithSettingsButtonElement.disabled = false;
+
+};
+
+var calculateCost = function () {
+
+    var checkedRadioBtn = document.querySelector("input[name='billItemTypeWithSettings']:checked");
+    if (checkedRadioBtn) {
+        var billItemType = checkedRadioBtn.value
+        if ((billItemType === "call") && (billWithSettingsInstance.updateTotalCost()) < (billWithSettingsInstance.upDateCritical())) {
+            billWithSettingsInstance.forEachCallAdd();
+        }
+        else if ((billItemType === "sms") && (billWithSettingsInstance.updateTotalCost()) < (billWithSettingsInstance.upDateCritical())) {
+            billWithSettingsInstance.forEachSmsAdd();
+        }
+
+    }
+    changeClass();
+    billWithSettingsButtonElementSwitch();
+    callTotalSettingsElement.innerHTML = billWithSettingsInstance.newCallTotal();
+    smsTotalSettingsElement.innerHTML = billWithSettingsInstance.newSmsTotal();
+    totalSettingsElement.innerHTML = billWithSettingsInstance.updateTotalCost();
+
+
+};
+var changeClass = function () {
+    totalSettingsElement.classList.remove("warning");
+    totalSettingsElement.classList.remove("critical");
+    totalSettingsElement.classList.add(billWithSettingsInstance.theWarningLevel());
+    totalSettingsElement.classList.add(billWithSettingsInstance.theCriticalLevel());
+
+};
+
+
+billWithSettingsButtonElement.addEventListener("click", calculateCost);
+var billWithSettingsButtonElementSwitch = function () {
+    if ((billWithSettingsInstance.updateTotalCost()) >= (billWithSettingsInstance.upDateCritical())) {
+        billWithSettingsButtonElement.disabled = true;
+    }
+
+};
+
+
--- factory-functions/bill-with-settings2.js
+++ factory-functions/bill-with-settings2.js
@@ -1,6 +1,6 @@
-let billWithSettingsFunction = function(){
- 
-    let callCostSetting =0;
+let billWithSettingsFunction = function () {
+
+    let callCostSetting = 0;
     let smsCostSetting = 0;
     let totalCallCost = 0;
     let totalSmsCost = 0;
@@ -8,101 +8,99 @@ let billWithSettingsFunction = function(){
     let warningSetting = 0;
     let criticalSetting = 0;
 
-    let setCallCost = function(callCost) {
+    let setCallCost = function (callCost) {
         callCostSetting = callCost;
     }
 
-    let updateCallCost = function() {
+    let updateCallCost = function () {
         return callCostSetting;
     }
-    
-    let setSmsCost = function(smsCost) {
+
+    let setSmsCost = function (smsCost) {
         smsCostSetting = smsCost;
     }
 
-    let updateSmsCost = function() {
+    let updateSmsCost = function () {
         return smsCostSetting;
     }
 
-    let forEachCallAdd = function(){
-        totalCallCost += callCostSetting ;
-    
+    let forEachCallAdd = function () {
+        totalCallCost += callCostSetting;
+
     }
 
-   let newCallTotal = function(){
-       return totalCallCost.toFixed(2);
-   }
-
-   let forEachSmsAdd = function(){
-        totalSmsCost += smsCostSetting ;
-      
+    let newCallTotal = function () {
+        return totalCallCost.toFixed(2);
     }
 
-   let newSmsTotal = function(){
-       return totalSmsCost.toFixed(2);
-   }
-   
-   
-   let updateTotalCost = function(){
-    return totalCallCost + totalSmsCost;
-   }
+    let forEachSmsAdd = function () {
+        totalSmsCost += smsCostSetting;
 
-   let stopSumming = function(){
-    if( updateTotalCost() > upDateCritical())
-    return setCriticalLevel();
-    
-   }
-
-   let setWarningLevel = function(warningValue){
-   warningSetting = warningValue;
-
-   }
-
-   let upDateWarning = function(){
-
-    return warningSetting;
-   }
-
-   let setCriticalLevel = function(criticalValue){
-    criticalSetting = criticalValue;
- 
-    }
- 
-    let upDateCritical = function(){
- 
-     return criticalSetting;
-    }
-    var theWarningLevel = function(){
-        if( updateTotalCost() >= upDateWarning() )
-        return "warning"
     }
 
-    var theCriticalLevel = function(){
-        if( updateTotalCost() >= upDateCritical() )
-        return "critical"
+    let newSmsTotal = function () {
+        return totalSmsCost.toFixed(2);
     }
 
- 
+
+    let updateTotalCost = function () {
+
+        return (totalCallCost + totalSmsCost).toFixed(2)
+
+
+    }
+
+    let setWarningLevel = function (warningValue) {
+        warningSetting = warningValue;
+
+    }
+
+    let upDateWarning = function () {
+
+        return warningSetting;
+    }
+
+    let setCriticalLevel = function (criticalValue) {
+        criticalSetting = criticalValue;
+
+    }
+
+    let upDateCritical = function () {
+
+        return criticalSetting;
+    }
+    var theWarningLevel = function () {
+        if (updateTotalCost() >= upDateWarning())
+            return "warning"
+    }
+
+    var theCriticalLevel = function () {
+        if (updateTotalCost() >= upDateCritical())
+            return "critical"
+    }
 
 
 
 
 
-return{
-    updateCallCost,
-    setCallCost,
-    updateSmsCost,
-    setSmsCost,
-    forEachCallAdd ,
-    newCallTotal,
-    forEachSmsAdd,
-    newSmsTotal,
-    updateTotalCost,
-    setWarningLevel,
-    upDateWarning ,
-    setCriticalLevel,
-    upDateCritical,
-    theWarningLevel,
-    theCriticalLevel,
-}
+
+
+    return {
+        updateCallCost,
+        setCallCost,
+        updateSmsCost,
+        setSmsCost,
+        forEachCallAdd,
+        newCallTotal,
+        forEachSmsAdd,
+        newSmsTotal,
+        updateTotalCost,
+        setWarningLevel,
+        upDateWarning,
+        setCriticalLevel,
+        upDateCritical,
+        theWarningLevel,
+        theCriticalLevel,
+
+    }
 };
